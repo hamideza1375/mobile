70b0e76d18a323db0b1304ae3289306fce9b3328
hamid reza <reza.attar1375@gmail.com>
2023-01-02 10:53:36 +0330

    Slider

--- src/screens/mobile/Home.js
+++ src/screens/mobile/Home.js
@@ -1,68 +1,24 @@
-import React, { useRef, useState } from 'react'
-import { Div, Img, ScrollHorizontal, Span, Button, Scroll, Icon, M_icon } from '../../Components/Html'
+import React from 'react'
+import { Span } from '../../Components/Html'
+import Slider from './Slider'
 
-var count = 1,
-  interval,
-  width
+
 
 function Home(p) {
 
-  const open = () => {
-    p.$ && p.$.id('scroll').scrollTo({ x: p.width * count, y: 0, animated: true });
-    count += 1
-  };
 
-  const right = () => {
-    if(count !== 0) count = count - 1
-    p.$ && p.$.id('scroll').scrollTo({ x: p.width * count, y: 0, animated: true });
-  };
-
-  const left = () => {
-    if(count !== 8) count += 1
-    p.$ && p.$.id('scroll').scrollTo({ x: p.width * count, y: 0, animated: true });
-  };
-
-
-  if (interval && p.width !== width) {
-    clearInterval(interval)
-  }
   return (
-    <Span>
-
-      <Span>
-        <ScrollHorizontal dir='ltr' id='scroll' onLayout={() => {
-          width = p.width
-          interval = setInterval(sum, 6000);
-          function sum() {
-            open()
-            if (count === 8) count = 0
-            if (p.width !== width) {
-              clearInterval(interval)
-            }
-          }
-        }} style={{ height: 260, overflow: 'hidden', flexWrap: 'wrap' }} >
-          <Img w={p.width} h={300} src={require('../../assets/images/a1.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a3.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a1.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a2.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a1.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a3.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a1.jpg')} />
-          <Img w={p.width} h={300} src={require('../../assets/images/a2.jpg')} />
-
-
-        </ScrollHorizontal>
-        <M_icon onClick={left} size={30} name="arrow-back-ios" style={{ position: 'absolute', zIndex: 10, left: 10, top: 130, color: '#222' }} />
-        <M_icon onClick={right} size={30} name="arrow-forward-ios" style={{ position: 'absolute', zIndex: 10, right: 10, top: 130, color: '#222' }} />
-      </Span>
-
-
-      <Span></Span>
-      <Span></Span>
-    </Span>
+      <Slider {...p} onClick={()=>{alert(8)}}
+      img1={require('../../assets/images/a1.jpg')}
+      img2={require('../../assets/images/a2.jpg')}
+      img3={require('../../assets/images/a3.jpg')}
+      img4={require('../../assets/images/a7.jpg')}
+      img5={require('../../assets/images/a1.jpg')}
+      img6={require('../../assets/images/a2.jpg')}
+      img7={require('../../assets/images/a3.jpg')}
+      img8={require('../../assets/images/a7.jpg')}
+      />
   )
-
-
 }
 
 export default Home
---
+++ src/screens/mobile/Slider.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Img, ScrollHorizontal, Span, M_icon, Press } from '../../Components/Html'
+
+var count = 1,
+  interval,
+  width,
+  plus,
+  minus
+
+function Slider(p) {
+
+  const open = () => {
+    { p.$ && p.$.id('scroll').scrollTo({ x: p.width * count, y: 0, animated: true }); }
+    if (count === 0) { plus = true; minus = false }
+    if (count === 7) { minus = true; plus = false }
+    if (minus) { count = count - 1 }
+    if (plus) { count += + 1 }
+  };
+
+  const right = () => {
+    if (count !== 0) count = count - 1
+    p.$ && p.$.id('scroll').scrollTo({ x: p.width * count, y: 0, animated: true });
+  };
+
+  const left = () => {
+    if (count !== 7) count += 1
+    p.$ && p.$.id('scroll').scrollTo({ x: p.width * count, y: 0, animated: true });
+  };
+
+
+  if (interval && p.width !== width) {
+    clearInterval(interval)
+  }
+  return (
+
+      <Span>
+        <ScrollHorizontal dir='ltr' id='scroll' onLayout={() => {
+          width = p.width
+          interval = setInterval(sum, 6000);
+          function sum() {
+            open()
+            if (p.width !== width) {
+              clearInterval(interval)
+            }
+          }
+        }} onClick={() => alert(7)} style={{ height: 260, width: p.width - 4, marginTop: 2, alignSelf: 'center', borderRadius: 5, overflow: 'hidden', flexWrap: 'wrap' }} >
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img1} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img2} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img3} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img4} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img5} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img6} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img7} /></Press>
+          <Press onClick={p.onClick} w={p.width} ><Img w='100%' h={300} src={p.img8} /></Press>
+        </ScrollHorizontal>
+        <M_icon onClick={left} size={30} name="arrow-back-ios" style={{ position: 'absolute', zIndex: 10, left: 10, top: 130, color: '#222' }} />
+        <M_icon onClick={right} size={30} name="arrow-forward-ios" style={{ position: 'absolute', zIndex: 10, right: 10, top: 130, color: '#222' }} />
+      </Span>
+  )
+}
+
+export default Slider
+
